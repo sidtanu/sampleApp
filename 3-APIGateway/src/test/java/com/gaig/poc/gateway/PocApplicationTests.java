ab93c9ff5b047d2924d9da822463091e4e3da3c8
SIDDHARTH MISHRA <59594643+mishra-siddharth@users.noreply.github.com>
2020-05-30 21:20:39 +0530

    Add files via upload

---
+++ 3-APIGateway/src/test/java/com/gaig/poc/gateway/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaig.poc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
